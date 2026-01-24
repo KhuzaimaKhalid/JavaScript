ff2bca81b924ee73acfc0ea1d5b075d92d53c31e
Khuzaima Khalid <khuzaimakhalidkhan@gmail.com>
2026-01-24 17:33:41 +0500

    lec 2 added

---
+++ lec2_arithmeticOperators/arithmeticOperators.js
@@ -0,0 +1,13 @@
+let students = 30
+console.log(students)
+students += 2
+console.log(students)
+students *= 2
+console.log(students)
+students **= 2
+console.log(students)
+students/=2
+console.log(students)
+
+let result = 6/2 ** (2+5)
+console.log(result)
